bc7d1a1f2e1736528f3ba9e9723699d902b7facf
DominikKossinski <dominikkossinski@gmail.com>
2022-11-23 22:41:41 +0100

    rest api docs

--- src/main/kotlin/pl/kossa/nasa/app/server/restcontrollers/ArticlesRestController.kt
+++ src/main/kotlin/pl/kossa/nasa/app/server/restcontrollers/ArticlesRestController.kt
@@ -1,5 +1,10 @@
 package pl.kossa.nasa.app.server.restcontrollers
 
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
@@ -14,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController
 import pl.kossa.nasa.app.server.architecture.BaseRestController
 import pl.kossa.nasa.app.server.db.data.Article
 import pl.kossa.nasa.app.server.db.data.NasaMediaType
+import pl.kossa.nasa.app.server.errors.NotFoundError
+import pl.kossa.nasa.app.server.errors.UnauthorizedError
 import pl.kossa.nasa.app.server.extensions.toApiString
 import pl.kossa.nasa.app.server.services.ArticlesService
 import java.util.*
@@ -26,6 +33,16 @@ class ArticlesRestController : BaseRestController() {
     private lateinit var articlesService: ArticlesService
 
     @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200"),
+            ApiResponse(
+                responseCode = "401",
+                description = "Unauthorized",
+                content = [Content(schema = Schema(implementation = UnauthorizedError::class))]
+            )
+        ]
+    )
     suspend fun getArticles(
         @RequestParam("from") @DateTimeFormat(pattern = "yyyy-MM-dd") from: Date,
         @RequestParam("to") @DateTimeFormat(pattern = "yyyy-MM-dd") to: Date
@@ -34,6 +51,21 @@ class ArticlesRestController : BaseRestController() {
     }
 
     @GetMapping("/{date}")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200"),
+            ApiResponse(
+                responseCode = "401",
+                description = "Unauthorized",
+                content = [Content(schema = Schema(implementation = UnauthorizedError::class))]
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "Unauthorized",
+                content = [Content(schema = Schema(implementation = NotFoundError::class))]
+            )
+        ]
+    )
     suspend fun getArticleByDate(@PathVariable("date") @DateTimeFormat(pattern = "yyyy-MM-dd") date: Date): Article {
         logger.info("GetArticle: date: $date ApiString ${date.toApiString()}")
         return articlesService.getArticleByDate(date)
