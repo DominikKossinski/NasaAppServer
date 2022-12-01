17942784f22416f62a4727f6c384ecb3791dfaef
DominikKossinski <dominikkossinski@gmail.com>
2022-12-01 22:36:13 +0100

    deleting saved articles

--- src/main/kotlin/pl/kossa/nasa/app/server/services/SavedArticlesRestController.kt
+++ src/main/kotlin/pl/kossa/nasa/app/server/services/SavedArticlesRestController.kt
@@ -1,13 +1,17 @@
 package pl.kossa.nasa.app.server.services
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import pl.kossa.nasa.app.server.architecture.BaseRestController
 import pl.kossa.nasa.app.server.db.data.Article
 import retrofit2.http.GET
+import java.util.*
 
 @RestController
 @RequestMapping("/api/saved-articles")
@@ -22,5 +26,11 @@ class SavedArticlesRestController : BaseRestController() {
         val user = getUserDetails()
         val savedArticles = savedArticleService.getByUserId(user.id)
         return savedArticles.map { it.article }
+    }
+
+    @DeleteMapping("/{date}")
+    suspend fun deleteSavedArticleByDate(@PathVariable("date") @DateTimeFormat(pattern = "yyyy-MM-dd") date: Date) {
+        val user = getUserDetails()
+        savedArticleService.deleteByUserIdAndDate(user.id, date)
     }
 }
